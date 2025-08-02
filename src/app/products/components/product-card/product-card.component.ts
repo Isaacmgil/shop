da74aafb878869e31e1432073eb42b1fbebdb4d3
Isaac Meneses <iantoniomgil@gmail.com>
2025-08-01 22:54:36 -0400

    Uploading repository

---
+++ src/app/products/components/product-card/product-card.component.ts
@@ -0,0 +1,16 @@
+import { Product } from '@/products/interfaces/product.interface';
+import { SlicePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'product-card',
+  imports: [RouterLink, SlicePipe],
+  templateUrl: './product-card.component.html',
+})
+export class ProductCardComponent {
+
+  product = input.required<Product>();
+
+
+}
---
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -0,0 +1,29 @@
+import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
+import { Component, inject } from '@angular/core';
+//import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
+import { ProductsService } from '../../../products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProductCardComponent],
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {
+
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({request}) => {
+
+      return this.productsService.getProducts({
+        limit: 1,
+      });
+    },
+  });
+
+
+ }
+
+
