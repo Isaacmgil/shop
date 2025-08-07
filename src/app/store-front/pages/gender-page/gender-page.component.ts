3a388ff7bef5415ecf3df6769ad6efe8bac8595b
Isaac Meneses <iantoniomgil@gmail.com>
2025-08-07 11:37:10 -0400

    Gender Products!

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,36 @@
 
-import { Component } from '@angular/core';
+import { ProductsService } from '@/products/services/products.service';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductCardComponent } from "@/products/components/product-card/product-card.component";
+
 @Component({
-  selector: 'app-gender-page',
-  imports: [],
+  selector: 'gender-page',
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+
+export class GenderPageComponent {
+
+  route = inject(ActivatedRoute);
+  gender = toSignal(
+    this.route.params.pipe(
+      map(({ gender }) => gender)
+    )
+  )
+
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    request: () => ({gender: this.gender()}),
+    loader: ({ request }) => {
+
+      return this.productsService.getProducts({
+        gender: request.gender,
+      });
+    },
+  });
+
+}
