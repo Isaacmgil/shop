55020b2fcb72e1a3d539020dcbd3b61b2cb9bcb5
Isaac Meneses <iantoniomgil@gmail.com>
2025-08-13 19:35:34 -0400

    FormValidation

---
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -0,0 +1,72 @@
+import { Product } from '@/products/interfaces/product.interface';
+import { Component, inject, input } from '@angular/core';
+import { ProductCarouselComponent } from "@/products/components/product-carousel/product-carousel.component";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@/form-utils';
+import { FormErrorLabelComponent } from "@/shared/components/form-error-label/form-error-label.component";
+
+@Component({
+  selector: 'app-product-details',
+  imports: [ProductCarouselComponent, ReactiveFormsModule, FormErrorLabelComponent],
+  templateUrl: './product-details.component.html',
+})
+export class ProductDetailsComponent {
+  product = input.required<Product>();
+
+  formBuilder = inject(FormBuilder);
+
+  productForm = this.formBuilder.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: ['men', [Validators.required, Validators.pattern('/men|women|kid|unisex/')]]
+
+
+  });
+
+
+  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  ngOnInit(): void {
+    this.setFormValue(this.product())
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+    this.productForm.reset(this.product() as any)
+    this.productForm.patchValue({tags: formLike.tags?.join(', ') });
+    // this.productForm.patchValue(formLike as any);
+  }
+
+  onSizeClicked(size: string){
+    const currentSizes = this.productForm.value.sizes ?? [];
+    if ( currentSizes.includes(size)){
+      currentSizes.splice(currentSizes.indexOf(size), 1);
+    }else {
+      currentSizes.push(size);
+    }
+
+    this.productForm.patchValue({sizes: currentSizes});
+  }
+
+
+  onSubmit(){
+    const isValid = this.productForm.valid;
+    this.productForm.markAllAsTouched
+
+    if(!isValid) return
+
+    const formValue = this.productForm.value;
+
+    const productLike: Partial<Product> = {
+      ...(formValue as any),
+      tags: formValue.tags?.toLowerCase().split(',').map(tag => tag.trim()) ?? [],
+    };
+
+    console.log({productLike})
+  }
+}
